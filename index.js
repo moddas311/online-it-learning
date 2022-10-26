1913cb588d2d234f61d02e8040f239dd3b954448
moddas311 <moddasir311@gmail.com>
2022-10-26 08:09:13 +0600

    cetegoris item added

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res)=>{
     res.send('Online IT Learning API Running');
+})
+
+app.get('/learning-categories', (req, res)=>{
+    res.send(categories);
 })
 
 app.listen(port, ()=>{
