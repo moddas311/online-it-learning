75330f2a4246b2bea106142882d17a6518d74c5c
moddas311 <moddasir311@gmail.com>
2022-10-26 07:36:50 +0600

    server running

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.send('Online IT Learning API Running');
+})
+
+app.listen(port, ()=>{
+    console.log('IT API running on port', port);
+})
