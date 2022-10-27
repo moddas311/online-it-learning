ac5c4558d0aad6bf2045b330d0db5951ddd52725
moddas311 <moddasir311@gmail.com>
2022-10-27 12:14:17 +0600

    server side done

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ app.use(cors());
 
 const categories = require('./data/categories.json');
 
-app.get('/', (req, res)=>{
+
+app.get('/', (req, res) => {
     res.send('Online IT Learning API Running');
 })
 
-app.get('/learning-categories', (req, res)=>{
+
+app.get('/learning-categories', (req, res) => {
     res.send(categories);
 })
 
-app.listen(port, ()=>{
+app.get('/learning-categories/:id', (req, res)=>{
+    const id = req.params.id
+    const selectedCategory = categories.find(category => category.id === id);
+    res.send(selectedCategory);
+})
+
+app.listen(port, () => {
     console.log('IT API running on port', port);
 })
